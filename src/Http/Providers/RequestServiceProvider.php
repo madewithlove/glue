0a39c975587beecc9a11767f0bfa598782de2d71
Hannes Van De Vreken <vandevreken.hannes@gmail.com>
2015-11-27 16:02:04 +0100

    Let ServerRequestFactory use superglobals by itself

--- src/Http/Providers/RequestServiceProvider.php
+++ src/Http/Providers/RequestServiceProvider.php
@@ -34,13 +34,7 @@ class RequestServiceProvider extends AbstractServiceProvider
     public function register()
     {
         $this->container->share(ServerRequestInterface::class, function () {
-            return ServerRequestFactory::fromGlobals(
-                $_SERVER,
-                $_GET,
-                $_POST,
-                $_COOKIE,
-                $_FILES
-            );
+            return ServerRequestFactory::fromGlobals();
         });
 
         $this->container->share(ResponseInterface::class, function () {
