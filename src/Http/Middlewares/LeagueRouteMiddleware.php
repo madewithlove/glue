64159092b305960d7844db81aa46850775051d41
Maxime Fabre <ehtnam6@gmail.com>
2015-11-27 17:49:28 +0100

    More precise type hint

--- src/Http/Middlewares/LeagueRouteMiddleware.php
+++ src/Http/Middlewares/LeagueRouteMiddleware.php
@@ -11,11 +11,11 @@
 namespace Madewithlove\Glue\Http\Middlewares;
 
 use League\Route\RouteCollection;
-use Psr\Http\Message\RequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface;
 use Relay\MiddlewareInterface;
 
-class LeagueRouteMiddleware implements MiddlewareInterface
+class LeagueRouteMiddleware
 {
     /**
      * @var RouteCollection
@@ -33,13 +33,13 @@ class LeagueRouteMiddleware implements MiddlewareInterface
     }
 
     /**
-     * @param Request                           $request
+     * @param ServerRequestInterface            $request
      * @param Response                          $response
      * @param callable|MiddlewareInterface|null $next
      *
      * @return Response
      */
-    public function __invoke(Request $request, Response $response, callable $next = null)
+    public function __invoke(ServerRequestInterface $request, Response $response, callable $next = null)
     {
         $response = $this->routes->dispatch($request, $response);
 
